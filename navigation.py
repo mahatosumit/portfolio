3617729ffdb16d5e78edf7a6ddd8bfa851d8ceb1
Sumit Mahato <91592750+mahatosumit@users.noreply.github.com>
2025-12-16 10:42:21 +0530

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -1,22 +1,6 @@
-python - <<EOF
-from picamera2 import Picamera2
-import cv2
-import time
-
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(
-    main={"size": (640, 480), "format": "RGB888"}
-))
-picam2.start()
-
-time.sleep(1)
-
-frame = picam2.capture_array()
-print(frame.shape)  # should be (480, 640, 3)
-
-cv2.imshow("Frame", frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-picam2.stop()
-EOF
+sudo apt install -y \
+gstreamer1.0-tools \
+gstreamer1.0-plugins-base \
+gstreamer1.0-plugins-good \
+gstreamer1.0-plugins-bad \
+gstreamer1.0-libav
