8306ce18f949fd541ff6f39fc1824f02c0971490
Sumit Mahato <91592750+mahatosumit@users.noreply.github.com>
2025-12-16 11:57:10 +0530

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -1,11 +1,6 @@
 import cv2
 
-gst = (
-    "v4l2src device=/dev/video8 ! "
-    "videoconvert ! "
-    "video/x-raw,width=640,height=480,framerate=30/1 ! "
-    "appsink drop=true"
-)
+gst = "v4l2src device=/dev/video8 ! videoconvert ! appsink"
 
 cap = cv2.VideoCapture(gst, cv2.CAP_GSTREAMER)
 print("Opened:", cap.isOpened())
@@ -13,7 +8,6 @@ print("Opened:", cap.isOpened())
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
-        print("Frame failed")
         break
     cv2.imshow("USB Cam", frame)
     if cv2.waitKey(1) & 0xFF == 27:
@@ -21,4 +15,3 @@ while cap.isOpened():
 
 cap.release()
 cv2.destroyAllWindows()
-
