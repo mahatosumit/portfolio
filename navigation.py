081ae9e64bfde334786bb67a66728b994aaa11ef
Sumit Mahato <91592750+mahatosumit@users.noreply.github.com>
2025-12-16 10:45:49 +0530

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -1,6 +1,14 @@
-sudo apt install -y \
-gstreamer1.0-tools \
-gstreamer1.0-plugins-base \
-gstreamer1.0-plugins-good \
-gstreamer1.0-plugins-bad \
-gstreamer1.0-libav
+sudo apt update
+sudo apt install -y code
+
+
+code --version
+
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/ms.gpg
+echo "deb [arch=arm64 signed-by=/usr/share/keyrings/ms.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update
+sudo apt install -y code
+
+
+code --disable-gpu
